42702bd82eb25775ea103c9660c26e767c985f65
nanes03 <inesmoalla68@gmail.com>
2024-02-28 18:54:08 +0100

    Message de commit

--- lib/app/modules/acceuil/views/settings.dart
+++ lib/app/modules/acceuil/views/settings.dart
@@ -4,11 +4,12 @@ import 'package:t/app/modules/acceuil/controllers/acceuil_controller.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class SettingsView extends GetView<AcceuilController> {
- SettingsView({Key? key}) : super(key: key);
+  SettingsView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Text(
@@ -23,68 +24,86 @@ class SettingsView extends GetView<AcceuilController> {
           ),
         ),
       ),
-      body: Container(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height -
-              MediaQuery.of(context).padding.top -
-              MediaQuery.of(context).padding.bottom -
-              kToolbarHeight -
-              kBottomNavigationBarHeight,
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 20),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.person,
-                          color: Colors.blue,
-                        ),
-                        SizedBox(width: 5),
-                        Text(
-                          "Account",
-                          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Divider(height: 20, thickness: 1),
-                  SizedBox(height: 20),
-                  buildAccountOption(context, "Change Password"),
-                  buildAccountOption(context, "Content Settings"),
-                  buildAccountOption(context, "Social"),
-                  buildAccountOption(context, "Language"),
-                  buildAccountOption(context, "Privacy and Security "),
-                  SizedBox(height: 40), // Ajout d'un espace entre les sections
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 20),
-                    child: Row(
-                      children: [
-                        Icon(Icons.volume_up_outlined, color: Colors.blue),
-                        SizedBox(width: 10),
-                        Text("Notifications",
-                            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-                      ],
-                    ),
-                  ),
-                  Divider(height: 20, thickness: 1),
-                  SizedBox(height: 20),
-                  buildNotificationOption(
-                      "theme Dark", controller.valNotify1.value, controller.onChangeFunction1),
-                  buildNotificationOption(
-                      "Notification 2", controller.valNotify2.value, controller.onChangeFunction2),
-                  buildNotificationOption(
-                      "Notification 3", controller.valNotify3.value, controller.onChangeFunction3),
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Card(
+              color: Colors.blue,
+              child: ListTile(
+                onTap: () {
+
+                },
+                title: Text(
+                  "Account",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold,color: Colors.white),
+                ),
+                trailing: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
+            const SizedBox(height: 10.0),
+            Card(
+              margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.lock_clock_outlined,color :Colors.blue),
+                 title: Text("Change Password"),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: (){
+
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.language,color :Colors.blue),
+                    title: Text("Change Language"),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: (){
+
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.location_pin,color :Colors.blue),
+                    title: Text("Change Location "),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: (){
+
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       bottomNavigationBar: GNav(
         backgroundColor: Colors.black,
         color: Colors.white,
@@ -103,65 +122,4 @@ class SettingsView extends GetView<AcceuilController> {
     );
   }
 
- Padding buildNotificationOption(String title, bool value, Function onChangeMethod) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w500,
-              color: Colors.grey.shade600,
-            ),
-          ),
-          Transform.scale(
-            scale: 0.7,
-            child: Switch(
-              activeColor: Colors.blue,
-              value: value,
-              onChanged: (bool? newValue) {
-                if (newValue != null) {
-                  onChangeMethod(newValue);
-                }
-              },
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  GestureDetector buildAccountOption(BuildContext context, String title) {
-    return GestureDetector(
-      onTap: () {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text(title),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text("Modifier le mot de passe"),
-                  Text("Gérer les notifications"),
-                  Text("Déconnexion"),
-                ],
-              ),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text("Fermer"),
-                ),
-              ],
-            );
-          },
-        );
-      },
-    );
-  }
 }
